5b8f07cbbb553606761be4881e7d247405d23779
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2024-02-24 10:56:07 +0100

    added entities for Data
    Updated Movies
    added Shows
    added Theaters

--- dotnetflix.Api/Data/Entities/Movie.cs
+++ dotnetflix.Api/Data/Entities/Movie.cs
@@ -1,24 +1,41 @@
-namespace dotnetflix.Api.Data.Entities;
 using System.ComponentModel.DataAnnotations;
 
-public class Movie
+namespace dotnetflix.Api.Data.Entities
 {
-    public int Id { get; set; }
+    public class Movie
+    {
+        public int Id { get; set; }
 
-    [Required]
-    public string Name { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
 
-    [Required]
-    public int Year { get; set; }
+        [Required]
+        [Range(1888, 2100)]
+        public int Year { get; set; }
 
-    [Required]
-    public string Description { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
 
-    [Required]
-    public double Rating { get; set; }
+        [Required]
+        public Rating Rating { get; set; }
 
-    [Required]
-    public int Runtime { get; set; }
+        [Required]
+        [Range(1, 500)]
+        public int Runtime { get; set; }
 
-    public string Image { get; set; }
+        public string Image { get; set; }
+    }
+
+    public enum Rating
+    {
+        AL = 1,
+        Six = 6,
+        Nine = 9,
+        Twelve = 12,
+        Sixteen = 16,
+        Eighteen = 18,
+        NA = 0
+    }
 }
