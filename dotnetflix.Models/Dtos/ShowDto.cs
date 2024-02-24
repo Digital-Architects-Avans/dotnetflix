79071c021ea5f5ef5686cb5b257369896eac8a8c
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-02-24 15:12:00 +0100

    created Dtos for entities

---
+++ dotnetflix.Models/Dtos/ShowDto.cs
@@ -0,0 +1,20 @@
+namespace dotnetflix.Models.Dtos;
+
+public class ShowDto
+{
+    public int Id { get; set; }
+    public int MovieId { get; set; }
+    public string Movie { get; set; }
+    public int TheaterId { get; set; }
+    public string Theater { get; set; }
+    public DateTime Date { get; set; }
+    public TimeSpan Time { get; set; }
+    public string Type { get; set; }
+    public decimal BasePrice { get; set; }
+}
+
+public enum MovieType
+{
+    Regular,
+    ThreeD,
+}
---
+++ dotnetflix.Models/Dtos/TheaterDto.cs
@@ -0,0 +1,8 @@
+namespace dotnetflix.Models.Dtos;
+
+public class TheaterDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Seats { get; set; }
+}
