d71f4690dee0dc84a554f817929533735ada8721
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-02-25 15:51:45 +0100

    added CRUD functionality to ShowController.cs

--- dotnetflix.Api/Controllers/ShowController.cs
+++ dotnetflix.Api/Controllers/ShowController.cs
@@ -1,50 +1,70 @@
 using dotnetflix.Api.Data.Entities;
 using dotnetflix.Api.Extensions;
+using dotnetflix.Api.Repositories.Movies;
 using dotnetflix.Api.Repositories.Shows;
-using dotnetflix.Models.Dtos;
+using dotnetflix.Api.Repositories.Theaters;
+using dotnetflix.Models.Dtos.Movie;
+using dotnetflix.Models.Dtos.Show;
+using dotnetflix.Models.Dtos.Theater;
+using Microsoft.AspNetCore.Mvc;
 
 namespace dotnetflix.Api.Controllers;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
 
 [Route("api/[controller]")]
 [ApiController]
-
 public class ShowController : ControllerBase
 {
-    private readonly IShowRepository showRepository;
-    public ShowController(IShowRepository showRepository)
+    private readonly IShowRepository _showRepository;
+    private readonly IMovieRepository _movieRepository;
+    private readonly ITheaterRepository _theaterRepository;
+
+    public ShowController(IShowRepository showRepository, IMovieRepository movieRepository, ITheaterRepository theaterRepository)
     {
-        this.showRepository = showRepository;
+        _showRepository = showRepository;
+        _movieRepository = movieRepository;
+        _theaterRepository = theaterRepository;
     }
+
     [HttpGet]
     public async Task<ActionResult<IEnumerable<ShowDto>>> GetShows()
     {
         try
         {
-            var shows = await this.showRepository.GetShows();
+            var shows = await _showRepository.GetShows();
             if (shows == null)
             {
                 return NotFound();
             }
-            else
+            
+            var movies = await _movieRepository.GetMovies();
+            if (movies == null)
             {
-                var showDtos = shows.ConvertToDto();
-                return Ok(showDtos);
+                throw new Exception("No movies exists in the system");
             }
+            
+            var theaters = await _theaterRepository.GetTheaters();
+            if (theaters == null)
+            {
+                throw new Exception("No theaters exists in the system");
+            }
+            
+
+            var showDtos = shows.ConvertToDto(movies, theaters);
+            return Ok(showDtos);
         }
         catch (Exception ex)
         {
-            return StatusCode(StatusCodes.Status500InternalServerError, 
+            return StatusCode(StatusCodes.Status500InternalServerError,
                 "Error retrieving data from the database");
         }
     }
+
     [HttpGet("{id}")]
     public async Task<ActionResult<ShowDto>> GetShow(int id)
     {
         try
         {
-            var show = await this.showRepository.GetShow(id);
+            var show = await _showRepository.GetShow(id);
             return Ok(show);
         }
         catch (Exception ex)
@@ -52,26 +72,33 @@ public class ShowController : ControllerBase
             return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
         }
     }
+
     [HttpPost]
-    public async Task<ActionResult<ShowDto>> AddShow(ShowDto showDto)
+    public async Task<ActionResult<ShowDto>> AddShow(AddShowDto addShowDto)
     {
         try
         {
-            var show = showDto.ConvertToEntity();
-            var newShow = await this.showRepository.AddShow(show);
-            return Ok(newShow);
+            var newShow = await _showRepository.AddShow(addShowDto);
+            if (newShow == null)
+            {
+                return NoContent();
+            }
+
+            var newShowDto = newShow.ConvertToDto();
+            return Ok(newShowDto);
         }
         catch (Exception ex)
         {
             return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
         }
     }
-    [HttpPut]
-    public async Task<ActionResult<ShowDto>> UpdateShow(Show show)
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult<ShowDto>> UpdateShow(int id, UpdateShowDto updateShowDto)
     {
         try
         {
-            var updatedShow = await this.showRepository.UpdateShow(show);
+            var updatedShow = await _showRepository.UpdateShow(id, updateShowDto);
             return Ok(updatedShow);
         }
         catch (Exception ex)
@@ -79,12 +106,13 @@ public class ShowController : ControllerBase
             return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
         }
     }
+
     [HttpDelete("{id}")]
     public async Task<ActionResult<ShowDto>> DeleteShow(int id)
     {
         try
         {
-            var show = await this.showRepository.DeleteShow(id);
+            var show = await _showRepository.DeleteShow(id);
             return Ok(show);
         }
         catch (Exception ex)
