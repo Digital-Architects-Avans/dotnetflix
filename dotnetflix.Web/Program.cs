da22b184eee0bd9bc7ce29ad088867b319215b79
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2024-02-25 17:29:04 +0100

    updated http builder

--- dotnetflix.Web/Program.cs
+++ dotnetflix.Web/Program.cs
@@ -7,10 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
-var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
-httpClient.DefaultRequestHeaders.Add("Access-Control-Allow-Origin", "*");
-
-builder.Services.AddScoped(sp => httpClient);
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
