7fef2e987aaf3ef0c98c8e3c7f4439f8c5d5c715
Van Hamsteren <vanhamsteren@outlook.com>
2024-02-25 21:23:26 +0100

    Added entities Ticket, Seat

---
+++ dotnetflix.Api/Data/Entities/Seat.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotnetflix.Api.Entities
+{
+	public class Seat
+	{
+		[Required]
+		public int Id { get; set; }
+		[Required]
+		public int Row {  get; set; }
+		[Required]
+		[ForeignKey(nameof(Theater.Id))]
+		public int TheaterId { get; set; }
+	}
+}
---
+++ dotnetflix.Api/Data/Entities/Ticket.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotnetflix.Api.Entities
+{
+    public class Ticket
+    {
+        // TICKET
+        [Required]
+        public int Id { get; set; }
+		
+        // MOVIE
+        [Required]
+		[ForeignKey(nameof(MovieId))]
+		public int MovieId { get; set; }
+        public string MovieName { get; set; }
+		public int MovieRuntime { get; set; }
+
+		// SHOW
+		[Required]
+		[ForeignKey(nameof(ShowId))]
+		public int ShowId { get; set; }
+		public DateTime ShowDate { get; set; }
+		public DateTime ShowTime { get; set; }
+        [Required]
+        public float BasePrice { get; set; }
+		public Discount Discount { get; set; }
+
+		// THEATER
+		[Required]
+		[ForeignKey(nameof(TheaterId))]
+		public int TheaterId { get; set; }
+
+        // SEAT
+        [Required]
+		[ForeignKey(nameof(SeatId))]
+		public int SeatId { get; set; }
+		[Required]
+		public int Row { get; set; }
+    }
+}
