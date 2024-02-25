85f2549b004ddf6f57409f9f1ed57fe4fbb079ff
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-02-25 15:52:06 +0100

    added CRUD functionality to MovieRepository.cs

--- dotnetflix.Api/Repositories/Movies/MovieRepository.cs
+++ dotnetflix.Api/Repositories/Movies/MovieRepository.cs
@@ -1,39 +1,77 @@
 using dotnetflix.Api.Data;
 using dotnetflix.Api.Data.Entities;
+using dotnetflix.Models.Dtos.Movie;
 using Microsoft.EntityFrameworkCore;
 
 namespace dotnetflix.Api.Repositories.Movies;
 
 public class MovieRepository: IMovieRepository
 {
-    private readonly DotNetFlixDbContext dotNetFlixDbContext;
+    private readonly DotNetFlixDbContext _dotNetFlixDbContext;
+    
     public MovieRepository(DotNetFlixDbContext dotNetFlixDbContext)
     {
-        this.dotNetFlixDbContext = dotNetFlixDbContext;
+        _dotNetFlixDbContext = dotNetFlixDbContext;
     }
+    
     public async Task<IEnumerable<Movie>> GetMovies()
     {
-        var movies = await this.dotNetFlixDbContext.Movies.ToListAsync();
+        var movies = await _dotNetFlixDbContext.Movies.ToListAsync();
         return movies;
     }
 
-    public Task<Movie> GetMovie(int id)
+    public async Task<Movie> GetMovie(int id)
     {
-        throw new NotImplementedException();
+        var movie = await _dotNetFlixDbContext.Movies.SingleOrDefaultAsync(m => m.Id == id);
+        return movie;
     }
 
-    public Task<Movie> AddMovie(Movie movie)
+    public async Task<Movie> AddMovie(AddMovieDto addMovieDto)
     {
-        throw new NotImplementedException();
+        var movie = new Movie
+        {
+            Title = addMovieDto.Title,
+            Year = addMovieDto.Year,
+            Description = addMovieDto.Description,
+            Rating = addMovieDto.Rating,
+            Runtime =  addMovieDto.Runtime,
+            Image = addMovieDto.Image
+        };
+        
+        var result = await _dotNetFlixDbContext.Movies.AddAsync(movie);
+        await _dotNetFlixDbContext.SaveChangesAsync();
+        return result.Entity;
     }
 
-    public Task<Movie> UpdateMovie(Movie movie)
+    public async Task<Movie> UpdateMovie(int id, UpdateMovieDto updateMovieDto)
     {
-        throw new NotImplementedException();
+        var movie = await _dotNetFlixDbContext.Movies.FindAsync(id);
+
+        if (movie != null)
+        {
+            movie.Title = updateMovieDto.Title;
+            movie.Year = updateMovieDto.Year;
+            movie.Description = updateMovieDto.Description;
+            movie.Rating = updateMovieDto.Rating;
+            movie.Runtime = updateMovieDto.Runtime;
+            movie.Image = updateMovieDto.Image;
+            await _dotNetFlixDbContext.SaveChangesAsync();
+            return movie;
+        }
+
+        return null;
     }
 
-    public Task<Movie> DeleteMovie(int id)
+    public async Task<Movie> DeleteMovie(int id)
     {
-        throw new NotImplementedException();
+        var movie = await _dotNetFlixDbContext.Movies.FindAsync(id);
+
+        if (movie != null)
+        {
+            _dotNetFlixDbContext.Movies.Remove(movie);
+            await _dotNetFlixDbContext.SaveChangesAsync();
+        }
+
+        return movie;
     }
 }
