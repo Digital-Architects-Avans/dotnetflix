7efca783d16e712be11fcb7b109b16ad0190b451
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-02-25 15:50:43 +0100

    moved enum type declaration to dotnetflix.Models

--- dotnetflix.Api/Data/Entities/Movie.cs
+++ dotnetflix.Api/Data/Entities/Movie.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using dotnetflix.Models.Dtos.Movie;
 
 namespace dotnetflix.Api.Data.Entities
 {
@@ -26,15 +27,5 @@ namespace dotnetflix.Api.Data.Entities
         public int Runtime { get; set; }
 
         public string Image { get; set; }
-    }
-
-    public enum Rating
-    {
-        G = 1,      // General audiences – All ages admitted
-        PG = 2,     // Parental guidance suggested – Some material may not be suitable for children
-        PG13 = 3,   // Parents strongly cautioned – Some material may be inappropriate for children under 13
-        R = 4,      // Restricted – Under 17 requires accompanying parent or adult guardian
-        NC17 = 5,   // No one 17 and under admitted
-        NA = 0      // Not Available/Not Applicable
     }
 }
