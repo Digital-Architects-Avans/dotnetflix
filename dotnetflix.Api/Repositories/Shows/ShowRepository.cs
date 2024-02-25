6d4cd626bd9bdea1a3cfa8fd8fb33b8ff5654175
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-02-25 15:51:35 +0100

    added CRUD functionality to ShowRepository.cs

--- dotnetflix.Api/Repositories/Shows/ShowRepository.cs
+++ dotnetflix.Api/Repositories/Shows/ShowRepository.cs
@@ -1,39 +1,89 @@
 using dotnetflix.Api.Data;
 using dotnetflix.Api.Data.Entities;
+using dotnetflix.Models.Dtos.Show;
 using Microsoft.EntityFrameworkCore;
 
 namespace dotnetflix.Api.Repositories.Shows;
 
-public class ShowRepository:IShowRepository
+public class ShowRepository : IShowRepository
 {
-    private readonly DotNetFlixDbContext dotNetFlixDbContext;
+    private readonly DotNetFlixDbContext _dotNetFlixDbContext;
+
     public ShowRepository(DotNetFlixDbContext dotNetFlixDbContext)
     {
-        this.dotNetFlixDbContext = dotNetFlixDbContext;
+        _dotNetFlixDbContext = dotNetFlixDbContext;
     }
+
     public async Task<IEnumerable<Show>> GetShows()
     {
-        var shows = await this.dotNetFlixDbContext.Shows.ToListAsync();
+        var shows = await _dotNetFlixDbContext.Shows.ToListAsync();
         return shows;
     }
 
     public Task<Show> GetShow(int id)
     {
-        throw new NotImplementedException();
+        var show = _dotNetFlixDbContext.Shows.SingleOrDefaultAsync(s => s.Id == id);
+        return show;
     }
 
-    public Task<Show> AddShow(Show show)
+    public async Task<Show> AddShow(AddShowDto addShowDto)
     {
-        throw new NotImplementedException();
+        var show = await (from movie in _dotNetFlixDbContext.Movies
+            where movie.Id == addShowDto.MovieId
+            from theater in _dotNetFlixDbContext.Theaters
+            where theater.Id == addShowDto.TheaterId
+            select new Show
+            {
+                MovieId = addShowDto.MovieId,
+                Movie = movie,
+                TheaterId = addShowDto.TheaterId,
+                Theater = theater,
+                Date = addShowDto.Date,
+                ScreenTime = addShowDto.ScreenTime,
+                Type = addShowDto.Type,
+                BasePrice = addShowDto.BasePrice
+            }).SingleOrDefaultAsync();
+
+        if (show != null)
+        {
+            var result = await _dotNetFlixDbContext.Shows.AddAsync(show);
+            await _dotNetFlixDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        return null;
     }
 
-    public Task<Show> UpdateShow(Show show)
+
+    public async Task<Show> UpdateShow(int id, UpdateShowDto updateShowDto)
     {
-        throw new NotImplementedException();
+        var show = await _dotNetFlixDbContext.Shows.FindAsync(id);
+        
+        if (show != null)
+        {
+            show.MovieId = updateShowDto.MovieId;
+            show.TheaterId = updateShowDto.TheaterId;
+            show.Date = updateShowDto.Date;
+            show.ScreenTime = updateShowDto.ScreenTime;
+            show.Type = updateShowDto.Type;
+            show.BasePrice = updateShowDto.BasePrice;
+            await _dotNetFlixDbContext.SaveChangesAsync();
+            return show;
+        }
+
+        return null;
     }
 
-    public Task<Show> DeleteShow(int id)
+    public async Task<Show> DeleteShow(int id)
     {
-        throw new NotImplementedException();
+        var show = await _dotNetFlixDbContext.Shows.FindAsync(id);
+
+        if (show != null)
+        {
+            _dotNetFlixDbContext.Shows.Remove(show);
+            await _dotNetFlixDbContext.SaveChangesAsync();
+        }
+
+        return show;
     }
 }
