65e751954f1cd14b19c9e0dfbef0ee4fea8da299
Paul van der Linden <vderlinden@gmail.com>
2024-02-26 13:10:13 +0100

    Revert "Coupled Show service to API"
    This reverts commit fa9a033ea5d81f89ed3f1f4523176a437c5527d1.

--- dotnetflix.Web/Services/Contracts/IShowService.cs
+++ dotnetflix.Web/Services/Contracts/IShowService.cs
@@ -1,8 +1,8 @@
-using dotnetflix.Models.Dtos.Show;
+// using dotnetflix.Models.Dtos;
 
 namespace dotnetflix.Web.Services.Contracts;
 
 public interface IShowService
 {
-    Task<IEnumerable<ShowDto>?> GetShows();
+    // Task<IEnumerable<ShowDto>> GetShows();
 }
