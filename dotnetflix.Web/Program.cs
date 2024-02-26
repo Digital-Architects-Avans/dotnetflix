d83d5028190be287e6496d699d4f3f2d21fa0d11
Paul van der Linden <vderlinden@gmail.com>
2024-02-26 17:21:37 +0100

    Update Program.cs of Web to localhost (temp)

--- dotnetflix.Web/Program.cs
+++ dotnetflix.Web/Program.cs
@@ -8,8 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7000/") });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5247/") });
 
 // Register services
 builder.Services.AddScoped<IMovieService, MovieService>();
