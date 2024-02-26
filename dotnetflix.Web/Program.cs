d7f41b42c61b538953008d396014ad2ffc4ab75f
Paul van der Linden <vderlinden@gmail.com>
2024-02-26 13:03:09 +0100

    Added EntityFrameworkAdapter for Grid component + Registered service

--- dotnetflix.Web/Program.cs
+++ dotnetflix.Web/Program.cs
@@ -10,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddFluentUIComponents();
+builder.Services.AddDataGridEntityFrameworkAdapter();
 
 await builder.Build().RunAsync();
