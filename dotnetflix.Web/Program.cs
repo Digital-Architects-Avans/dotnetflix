2ac704febc190234e516e024d28daa18b32c4bf5
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-02-26 17:38:39 +0100

    settings for https

--- dotnetflix.Web/Program.cs
+++ dotnetflix.Web/Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7100/") });
 // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7000/") });
 
 // Register services
