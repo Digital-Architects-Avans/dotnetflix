dc5ddbed4840ef995b605f1ca3dc8d34c0c6826e
Paul van der Linden <vderlinden@gmail.com>
2024-02-26 12:58:54 +0100

    Register FluentUI component services after HttpClient

--- dotnetflix.Web/Program.cs
+++ dotnetflix.Web/Program.cs
@@ -1,11 +1,14 @@
 using dotnetflix.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddFluentUIComponents();
 
 await builder.Build().RunAsync();
