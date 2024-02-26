363f5b7b99817d52dd90e32d5ec6b093459d4a9f
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2024-02-27 00:23:12 +0100

    added theaters and shows cruds

--- dotnetflix.Models/Dtos/Show/AddShowDto.cs
+++ dotnetflix.Models/Dtos/Show/AddShowDto.cs
@@ -3,7 +3,9 @@ namespace dotnetflix.Models.Dtos.Show;
 public class AddShowDto
 {
     public int MovieId { get; set; }
+    public string MovieTitle { get; set; }
     public int TheaterId { get; set; }
+    public string TheaterName { get; set; }
     public DateTime Date { get; set; }
     public int ScreenTime { get; set; }
     public MovieType Type { get; set; }
