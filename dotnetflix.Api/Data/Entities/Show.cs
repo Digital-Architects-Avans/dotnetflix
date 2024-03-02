23c27cb080c48f5b614979deed29bca43f4b953a
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-03-02 14:05:24 +0100

    remoddeled Show entity

--- dotnetflix.Api/Data/Entities/Show.cs
+++ dotnetflix.Api/Data/Entities/Show.cs
@@ -7,15 +7,15 @@ namespace dotnetflix.Api.Data.Entities
     public class Show
     {
         public int Id { get; set; }
-
-        [Required]
-        [ForeignKey(nameof(Theater.Id))]
-        public int TheaterId { get; set; }
     
         [Required]
         [ForeignKey(nameof(Movie.Id))]
         public int MovieId { get; set; }
-
+        
+        [Required]
+        [ForeignKey(nameof(Theater.Id))]
+        public int TheaterId { get; set; }
+        
         [Required]
         public DateTime Date { get; set; }
 
@@ -24,13 +24,17 @@ namespace dotnetflix.Api.Data.Entities
 
         [Required]
         public MovieType Type { get; set; }
-
+        
         [Required]
         public decimal BasePrice { get; set; }
         
         // Navigation properties
-        public Theater Theater { get; set; }
+        // The Show entity has a one-to-many relationship with the Ticket entity
+        // The Show entity has a many-to-one relationship with the Movie entity
         public Movie Movie { get; set; }
+        
+        public Theater Theater { get; set; }
+        
         public ICollection<Ticket> Tickets { get; set; }
     }
     
