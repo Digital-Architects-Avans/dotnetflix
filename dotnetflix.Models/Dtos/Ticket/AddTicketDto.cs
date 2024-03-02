7f551c3bf2db890a7527cd2250a90e6a3397a27c
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-03-02 13:57:16 +0100

    Fixed AddTicketDto.cs

---
+++ dotnetflix.Models/Dtos/Ticket/AddTicketDto.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnetflix.Models.Dtos.Ticket;
+
+public class AddTicketDto
+{
+	public int ShowId { get; set; }
+	public int SeatId { get; set; }
+	public Discount Discount { get; set; }
+	public decimal TicketPrice { get; set; }
+}
