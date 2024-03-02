c1b6cc77c22e92c10189e4be7fef64af4a93652f
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-03-02 14:07:51 +0100

    remodeled TheaterRow.cs

--- dotnetflix.Api/Data/Entities/TheaterRow.cs
+++ dotnetflix.Api/Data/Entities/TheaterRow.cs
@@ -1,15 +1,23 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using dotnetflix.Api.Entities;
 
-namespace dotnetflix.Api.Data.Entities;
-
-public class TheaterRow
+namespace dotnetflix.Api.Data.Entities
 {
-    [Key]
-    public int Id { get; set; }
-    public int TheaterId { get; set; }
-    [ForeignKey("TheaterId")]
-    public virtual Theater Theater { get; set; }
-    public int RowNumber { get; set; }
-    public int NumberOfSeats { get; set; }
+    public class TheaterRow
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Theater.Id))] 
+        public int TheaterId { get; set; }
+
+        public int RowNumber { get; set; }
+
+        public int NumberOfSeats { get; set; }
+
+        public ICollection<Seat> Seats { get; set; }
+
+        public Theater Theater { get; set; }
+    }
 }
