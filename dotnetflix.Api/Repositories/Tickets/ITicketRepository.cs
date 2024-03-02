10ff2121aa27307e097989c01abf01e7071ae952
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-03-02 14:00:15 +0100

    Created Repository ITicketRepository.cs.cs

--- dotnetflix.Api/Repositories/Tickets/ITicketRepository.cs
+++ dotnetflix.Api/Repositories/Tickets/ITicketRepository.cs
@@ -1,21 +1,14 @@
 ﻿using dotnetflix.Api.Data.Entities;
 using dotnetflix.Api.Entities;
+using dotnetflix.Models.Dtos.Ticket;
 
 namespace dotnetflix.Api.Repositories.Contracts;
 
 public interface ITicketRepository
 {
-    // FETCH LISTS OF OBJECTS
     Task<IEnumerable<Ticket>> GetTickets();
-    Task<IEnumerable<Movie>> GetMovies();
-    Task<IEnumerable<Show>> GetShows();
-    Task<IEnumerable<Theater>> GetTheaters();
-	Task<IEnumerable<Seat>> GetSeats();
-
-	// GET INDIVIDUAL ITEMS
-	Task<Ticket> GetTicket(int id);
-    Task<Movie> GetMovie(int id);
-    Task<Show> GetShow(int id);
-    Task<Theater> GetTheater(int id);
-	Task<Seat> GetSeat(int id);
+	Task<Ticket?> GetTicket(int id);
+	Task<Ticket> AddTicket(AddTicketDto addTicketDto);
+	Task<Ticket?> UpdateTicket(int id, UpdateTicketDto updateTicketDto);
+	Task<bool> DeleteTicket(int id);
 }
