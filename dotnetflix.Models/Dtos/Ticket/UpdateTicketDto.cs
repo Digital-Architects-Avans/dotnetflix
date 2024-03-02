c660df9d21dd73e32d9fe74982ff12714ac57fd2
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-03-02 14:10:15 +0100

    Remodelled UpdateTicketDto.cs

--- dotnetflix.Models/Dtos/Ticket/UpdateTicketDto.cs
+++ dotnetflix.Models/Dtos/Ticket/UpdateTicketDto.cs
@@ -6,13 +6,10 @@ using System.Threading.Tasks;
 
 namespace dotnetflix.Models.Dtos.Ticket;
 
-public class UpdateTicket
+public class UpdateTicketDto
 {
-	public int Id { get; set; }
 	public int ShowId { get; set; }
-	public int OrderId { get; set; }
-	public int Row { get; set; }
 	public int SeatId { get; set; }
 	public Discount Discount { get; set; }
-	public decimal BasePrice { get; set; }
+	public decimal TicketPrice { get; set; }
 }
