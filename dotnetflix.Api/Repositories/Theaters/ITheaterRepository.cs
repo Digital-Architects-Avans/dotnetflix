9ba2dbdca66912a83f0e9e3cbd8bd959d5063694
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-03-02 13:59:50 +0100

    Fixed possible null reference

--- dotnetflix.Api/Repositories/Theaters/ITheaterRepository.cs
+++ dotnetflix.Api/Repositories/Theaters/ITheaterRepository.cs
@@ -6,8 +6,8 @@ namespace dotnetflix.Api.Repositories.Theaters;
 public interface ITheaterRepository
 {
     Task<IEnumerable<Theater>> GetTheaters();
-    Task<Theater> GetTheater(int id);
+    Task<Theater?> GetTheater(int id);
     Task<Theater> AddTheater(AddTheaterDto addTheaterDto);
-    Task<Theater> UpdateTheater(int id, UpdateTheaterDto updateTheaterDto);
-    Task<Theater> DeleteTheater(int id);
+    Task<Theater?> UpdateTheater(int id, UpdateTheaterDto updateTheaterDto);
+    Task<bool> DeleteTheater(int id);
 }
