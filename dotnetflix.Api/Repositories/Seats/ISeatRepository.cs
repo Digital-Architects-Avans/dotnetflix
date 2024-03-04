1706017af74ee21bf519ab81f374e5cc7a387d97
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-03-04 18:40:47 +0100

    created repository for seat db operations

---
+++ dotnetflix.Api/Repositories/Seats/ISeatRepository.cs
@@ -0,0 +1,16 @@
+using dotnetflix.Api.Data.Entities;
+using dotnetflix.Api.Entities;
+using dotnetflix.Models.Dtos.Movie;
+using dotnetflix.Models.Dtos.Seat;
+
+namespace dotnetflix.Api.Repositories.Seats;
+
+public interface ISeatRepository
+{
+    Task<IEnumerable<Seat>> GetSeats();
+    Task<IEnumerable<Seat>> GetSeatsForTheater(int theaterId);
+    Task<Seat?> GetSeat(int id);
+    Task<Seat> AddSeat(AddSeatDto addSeatDto);
+    Task<Seat?> UpdateSeat(int id, UpdateSeatDto updateSeatDto);
+    Task<bool> DeleteSeat(int id);
+}
