1482a084517c591c9d0f9a268300d27ff2949325
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-03-07 19:24:04 +0100

    added SpokenLanguage string property

--- dotnetflix.Models/Dtos/Movie/MovieDto.cs
+++ dotnetflix.Models/Dtos/Movie/MovieDto.cs
@@ -8,6 +8,7 @@ public class MovieDto
     public string Description { get; set; }
     public Rating Rating { get; set; }
     public int Runtime { get; set; }
+    public string SpokenLanguages { get; set; }
     public string Image { get; set; }
     
 }
