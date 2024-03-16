c89c0e1e37f662a0ebf45549b231de6acdfec10a
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2024-03-16 01:22:17 +0100

    adjusted trustpilotservice to interact with api

--- dotnetflix.Web/Services/Contracts/ITrustpilotService.cs
+++ dotnetflix.Web/Services/Contracts/ITrustpilotService.cs
@@ -1,18 +1,12 @@
+using dotnetflix.Models.Dtos.Review;
+
 namespace dotnetflix.Api.Services.Contracts;
 
 public interface ITrustpilotService
 {
-    Task<TrustpilotReviews> GetReviewsAsync();
+    Task<IEnumerable<ReviewDto>> GetReviews();
+    Task<AddReviewDto> AddReview(AddReviewDto addReviewDto);
 }
 
-public class TrustpilotReviews
-{
-    public TrustpilotReview[] Reviews { get; set; }
-}
 
-public class TrustpilotReview
-{
-    public string Name { get; set; }
-    public string ReviewText { get; set; }
-    public int Stars { get; set; }
-}
+
--- dotnetflix.Web/Services/MockTrustpilotService.cs
+++ dotnetflix.Web/Services/MockTrustpilotService.cs
@@ -1,28 +1,64 @@
+using System.Net.Http.Json;
 using dotnetflix.Api.Services.Contracts;
+using dotnetflix.Models.Dtos.Review;
 
 namespace dotnetflix.Api.Services;
 
 public class MockTrustpilotService : ITrustpilotService
 {
-    public Task<TrustpilotReviews> GetReviewsAsync()
-    {
-        return Task.FromResult(new TrustpilotReviews
-        {
-            Reviews = new[]
-            {
-                new TrustpilotReview
-                {
-                    Name = "Karl",
-                    ReviewText = "Toffe film wel!",
-                    Stars = 5
-                },
-                new TrustpilotReview
-                {
-                    Name = "Karen",
-                    ReviewText = "De popcorn plakt aan je vingers",
-                    Stars = 2
-                }
-            }
-        });
-    }
+   private readonly HttpClient _httpClient;
+   
+   public MockTrustpilotService(HttpClient httpClient)
+   {
+       _httpClient = httpClient;
+   }
+
+   public async Task<IEnumerable<ReviewDto>> GetReviews()
+   {
+       try
+       {
+           var response = await _httpClient.GetAsync("api/Review/GetReviews");
+           if (response.IsSuccessStatusCode)
+           {
+               if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+               {
+                   return default(IEnumerable<ReviewDto>);
+               }
+
+               return await response.Content.ReadFromJsonAsync<IEnumerable<ReviewDto>>();
+           }
+           else
+           {
+               var message = await response.Content.ReadAsStringAsync();
+               throw new Exception(message);
+           }
+       }
+       catch (Exception e)
+       {
+           Console.WriteLine($"An error occurred: {e.Message}");
+           throw;
+       }
+   }
+
+   public Task<AddReviewDto> AddReview(AddReviewDto newReview)
+   {
+       var addReviewDto = new AddReviewDto
+       {
+           Name = newReview.Name,
+           Stars = newReview.Stars,
+           ReviewText = newReview.ReviewText
+       };
+       
+       var response = _httpClient.PostAsJsonAsync("api/Review", addReviewDto);
+       
+       if (response.Result.IsSuccessStatusCode)
+       {
+           return Task.FromResult(addReviewDto);
+       }
+       else
+       {
+           var message = response.Result.Content.ReadAsStringAsync();
+           throw new Exception(message.Result);
+       }
+   }
 }
