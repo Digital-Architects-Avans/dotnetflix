4ccfe27245f7a896314c639e8d90261635594261
Paul van der Linden <vderlinden@gmail.com>
2024-03-16 12:39:23 +0100

    Add Supplement entity

---
+++ dotnetflix.Api/Data/Entities/Supplement.cs
@@ -0,0 +1,10 @@
+namespace dotnetflix.Api.Data.Entities;
+
+public class Supplement
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+}
