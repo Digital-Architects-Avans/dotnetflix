3fca7ab9b99acc6864b7121a3e2590c5b2fc4176
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-03-17 18:42:34 +0100

    add supplement collection

--- dotnetflix.Models/Dtos/Ticket/AddTicketDto.cs
+++ dotnetflix.Models/Dtos/Ticket/AddTicketDto.cs
@@ -9,5 +9,4 @@ public class AddTicketDto
 	public int TicketTypeId { get; set; }
 	public decimal TicketPrice { get; set; }
 	public int? OrderId { get; set; }
-	public ICollection<int> Supplements { get; set; }
 }
